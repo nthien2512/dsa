99728b3a91a99cbec9729d8f295d39c8d1a7f12b
ntdev204 <ntdev.npc204@gmail.com>
2024-05-26 15:09:37 +0700

    Refactor heapSort function for improved efficiency and readability

--- Thi/sapxep/heapSort.cpp
+++ Thi/sapxep/heapSort.cpp
@@ -18,3 +18,16 @@ void heapify(int arr[], int n, int i) {
     heapify(arr, n, largest);
   }
 }
+
+void heapSort(int arr[], int n) {
+  for (int i = n / 2 - 1; i >= 0; i--) {
+    heapify(arr, n, i);
+  }
+
+  for (int i = n - 1; i >= 0; i--) {
+    int temp = arr[0];
+    arr[0] = arr[i];
+    arr[i] = temp;
+    heapify(arr, i, 0);
+  }
+}
